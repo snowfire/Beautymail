a52b549f33d00a1235f53aec84c44aecce7bbbf0
Ann Furbäck <annfurback@Anns-MacBook-Pro.local>
2015-07-29 15:22:49 +0200

    New theme sunny

---
+++ src/views/templates/sunny/button.blade.php
@@ -0,0 +1,18 @@
+<tr>
+    <td class="w50" width="50"></td>
+    <td class="w560" width="560">
+        <table class="w560" border="0" cellpadding="0" cellspacing="0" width="560">
+            <tbody>
+            <tr class="large_only"><td class="w560" height="15" width="560"></td></tr>
+            <tr>
+                <td class="w560" width="560">
+                    <div class="button-content" align="center">
+                        <button>{{ $title }}</button>
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </td>
+    <td class="w30" width="30"></td>
+</tr>
