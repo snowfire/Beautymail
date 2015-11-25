ec3f56b034334a8e27f68903131610de67cab0f6
Emil Sundberg <emil@snowfire.net>
2015-11-25 11:46:11 +0100

    Support for custom CSS to override default rules

--- src/config/settings.php
+++ src/config/settings.php
@@ -2,22 +2,33 @@
 
 return [
 
+    // These CSS rules will be applied after the regular template CSS
+
+    /*
+        'css' => [
+            '.button-content .button { background: red }',
+        ],
+    */
+
     'colors' => [
 
         'highlight' => '#004ca3',
         'button'    => '#004cad',
 
     ],
+
     'view' => [
         'senderName'  => null,
         'reminder'    => null,
         'unsubscribe' => null,
         'address'     => null,
+
         'logo'        => [
             'path'   => '%PUBLIC%/vendor/Beautymail/assets/images/sunny/logo.png',
             'width'  => '',
             'height' => '',
         ],
+
         'twitter'  => null,
         'facebook' => null,
         'flickr'   => null,
