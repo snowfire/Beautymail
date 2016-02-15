d3d89cd57f1bbaf4ed652a9d45c4a609e92c5178
Ann Furbäck <ann@snowfire.net>
2016-02-15 13:53:54 +0100

    Increases padding top for sunny template

--- src/views/templates/sunny/contentStart.blade.php
+++ src/views/templates/sunny/contentStart.blade.php
@@ -4,6 +4,7 @@
 		<table class="w560" border="0" cellpadding="0" cellspacing="0" width="560">
 			<tbody>
 			<tr><td class="w560" height="15" width="560"></td></tr>
+			<tr><td class="w560" height="15" width="560"></td></tr>
 			<tr>
 				<td class="w560" width="560">
 					<div class="article-content" align="left">
