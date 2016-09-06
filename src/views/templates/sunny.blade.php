1584347eef4073e6e889b3f8b72a4630b25ff262
Joshua de Gier <info@pendo.nl>
2016-09-06 16:42:04 +0200

    $css variable in sunny template

--- src/views/templates/sunny.blade.php
+++ src/views/templates/sunny.blade.php
@@ -3,9 +3,11 @@
 		<title>{{ $senderName or '' }}</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<style type="text/css">{{ file_get_contents(app_path() . '/../vendor/snowfire/beautymail/src/styles/css/sunny.css') }}</style>
+		@if($css))
 		<style type="text/css">
-			{{ $css or '' }
+			{{ $css or '' }}
 		</style>
+		@endif
 	</head>
 	<body>
 	<table id="background-table" border="0" cellpadding="0" cellspacing="0" width="100%">
