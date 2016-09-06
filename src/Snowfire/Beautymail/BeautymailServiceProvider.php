4ae7340101fe12263f36227d921f1b822493fbd0
Joshua de Gier <info@pendo.nl>
2016-09-06 16:39:15 +0200

    Includes the CSS as setting in Beautymail
    This makes the $css variable useable in all templates

--- src/Snowfire/Beautymail/BeautymailServiceProvider.php
+++ src/Snowfire/Beautymail/BeautymailServiceProvider.php
@@ -42,7 +42,10 @@ class BeautymailServiceProvider extends ServiceProvider
     {
         $this->app->singleton('Snowfire\Beautymail\Beautymail',
             function ($app) {
-                return new \Snowfire\Beautymail\Beautymail(config('beautymail.view'));
+                return new \Snowfire\Beautymail\Beautymail(array_merge(
+                    config('beautymail.view'),
+                    ['css' => implode("\n", config('beautymail.css'))]
+                );
             });
     }
 
