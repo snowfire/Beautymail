558b38b45bc7c8efec36921e99e9b03a18bf22d2
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2017-01-30 06:00:02 -0500

    Fix uppercase in settings.php (#41)
    This was breaking the test email image links.

--- src/config/settings.php
+++ src/config/settings.php
@@ -24,7 +24,7 @@ return [
         'address'     => null,
 
         'logo'        => [
-            'path'   => '%PUBLIC%/vendor/Beautymail/assets/images/sunny/logo.png',
+            'path'   => '%PUBLIC%/vendor/beautymail/assets/images/sunny/logo.png',
             'width'  => '',
             'height' => '',
         ],
