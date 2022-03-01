607a47382c0661670acd4b048741b8f7bb941650
lukepower <lukas@lukepower.com>
2022-03-01 08:13:08 +0100

    Update BeautymailServiceProvider.php
    Removed CssInlinerPlugin

--- src/Snowfire/Beautymail/BeautymailServiceProvider.php
+++ src/Snowfire/Beautymail/BeautymailServiceProvider.php
@@ -30,11 +30,11 @@ class BeautymailServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(__DIR__.'/../../views', 'beautymail');
 
-        try {
+        /**try {
             $this->app['mailer']->getSwiftMailer()->registerPlugin(new CssInlinerPlugin());
         } catch (\Exception $e) {
             \Log::debug('Skipped registering SwiftMailer plugin: CssInlinerPlugin.');
-        }
+        }**/
     }
 
     /**
